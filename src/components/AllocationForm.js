a4e4c15349478711132f9b1a6699a03f56a63674
Sam(Islam) <islam.alsaeed@gmail.com>
2023-12-17 11:26:41 +0200

    made budget field editable

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -46,7 +46,7 @@ const AllocationForm = (props) => {
                         <option value="Marketing" name="marketing"> Marketing</option>
                 <option value="Sales" name="sales">Sales</option>
                 <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
+                <option value="Human Resource" name="hr">HR</option>
                 <option value="IT" name="it">IT</option>
                 <option value="Admin" name="admin">Admin</option>
                   </select>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,40 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
-    }
+    };
+
+    const handleSaveBudget = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: parseInt(newBudget),
+        });
+    };
+
+    const handleIncreaseBudget = () => {
+        setNewBudget((prevBudget) => prevBudget + 10);
+    };
+
+    const handleDecreaseBudget = () => {
+        setNewBudget((prevBudget) => prevBudget - 10);
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span style={{marginRight:"1rem"}}>Budget: £{budget}</span>
+                <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} style={{marginLeft:'2rm'}}></input>
+            
+            <div style={{ marginTop: '1rem' }}>
+                <button className="btn btn-success" onClick={handleIncreaseBudget}>Increase by 10</button>
+                <button className="btn btn-danger" onClick={handleDecreaseBudget} style={{ marginLeft: '1rem' }}>Decrease by 10</button>
+                <button className="btn btn-primary" onClick={handleSaveBudget} style={{ marginLeft: '1rem' }}>Save</button>
+            </div>
+        </div>
     );
 };
+
 export default Budget;
