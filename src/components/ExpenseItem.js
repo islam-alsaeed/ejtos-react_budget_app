bf6321d58add56e7ffbb4f6809be4a71d20f9a5b
Sam(Islam) <islam.alsaeed@gmail.com>
2023-12-19 21:03:03 +0200

    add reduce button in allocation list

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import './ExpenseItem.css'; // Import your CSS file
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -20,17 +21,45 @@ const ExpenseItem = (props) => {
 
         dispatch({
             type: 'ADD_EXPENSE',
-            payload: expense
+            payload: expense,
         });
+    };
 
-    }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'SUB_EXPENSE',
+            payload: expense,
+        });
+    };
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>£{props.cost}</td>
+            <td>
+                <button
+                    className={`circle-button increase-button`}
+                    onClick={(event) => increaseAllocation(props.name)}
+                >
+                    +
+                </button>
+            </td>
+            <td>
+                <button
+                    className={`circle-button decrease-button`}
+                    onClick={(event) => decreaseAllocation(props.name)}
+                >
+                    -
+                </button>
+            </td>
+            <td>
+                <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
+            </td>
         </tr>
     );
 };
